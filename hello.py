2b6ec690625fbbbe641729fcd9f2101945b421ab
ljz <ljz@ljz.com>
2018-11-05 22:41:08 +0800

    ‘测试提交

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ matchObj = re.match( r'dogs', line, re.M|re.I)
 if matchObj:
    print ("match --> matchObj.group() : ", matchObj.group())
 else:
-   print ("No match!!")
+   print ("No match!!!!")
  
 matchObj = re.search( r'dogs', line, re.M|re.I)
 if matchObj:
