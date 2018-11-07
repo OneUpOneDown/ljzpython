ad2461a9833a48f23ca68980acdee179b866ce94
ljz <ljz@ljz.com>
2018-11-07 22:56:16 +0800

    LOL

--- ADB/dy.py
+++ ADB/dy.py
@@ -13,6 +13,15 @@ import re
 #         #在fiddler中刚刚看到每一个视频的所有信息
 #         # 都在aweme_list中
 #         video_url=data['aweme_list']
+# GET https://aweme.snssdk.com/aweme/v1/feed/?type=0&max_cursor=0&min_cursor=-1&count=6&volume=0.0&pull_type=2&need_relieve_aweme=0&ts=1541592345&app_type=lite&os_api=22&device_type=OPPO%20A53m&device_platform=android&ssmix=a&iid=49176391802&manifest_version_code=179&dpi=320&uuid=860793037946646&version_code=179&app_name=aweme&version_name=1.7.9&openudid=a7cff120682f5086&device_id=58850191695&resolution=720*1200&os_version=5.1.1&language=zh&device_brand=OPPO&ac=wifi&update_version_code=1790&aid=1128&channel=sem_baidu_dy_pz&_rticket=1541592345802&as=a1e5bd7e0921ab35a21310&cp=d112bb569e25e555e1awkc&mas=003184703ef31d6d987579202f6fb009e00c8ccc8c4c86accc4686 HTTP/1.1
+# Host: aweme.snssdk.com
+# Connection: keep-alive
+# Cookie: odin_tt=6f5a3571334b43413065424e5253464c8a64efe60875ad1c81e7e3b7f0ae3ad44d354453942e78f220192b3fc2ac6c9d; qh[360]=1; install_id=49176391802; ttreq=1$bd72a992adaec8e4546c6e7c076514a34434825e
+# Accept-Encoding: gzip
+# X-SS-REQ-TICKET: 1541592345797
+# X-SS-TC: 0
+# User-Agent: com.ss.android.ugc.aweme/310 (Linux; U; Android 5.1.1; zh_CN; OPPO A53m; Build/LMY47V; Cronet/58.0.2991.0)
+
 #         path='D:/爬虫数据/douyin'
 #         if not os.path.exists(path):
 #             os.mkdir(path)
